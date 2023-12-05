3a190f79d0924088a42c5bcda8a01d4678941c66
j-lgrant <justingrant373@gmail.com>
2023-12-05 14:48:29 +0000

    spotify-myVersion

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'lymfauxfasiwbxmpdghz.supabase.co'
+    ]
+  }
+}
 
 module.exports = nextConfig
+
+
