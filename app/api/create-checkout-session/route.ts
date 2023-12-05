3eeaf0c75b2d1f08863e77c24d679433a8efbb8e
j-lgrant <justingrant373@gmail.com>
2023-12-05 16:42:16 +0000

    updatedChange

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -1,3 +1,53 @@
+// import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+// import { cookies } from "next/headers";
+// import { NextResponse } from 'next/server';
+
+// import { stripe } from '@/libs/stripe';
+// import { getURL } from '@/libs/helpers';
+// import { createOrRetrieveCustomer } from '@/libs/supabaseAdmin';
+
+// export async function POST(
+//   request: Request
+// ) {
+//   const { price, quantity = 1, metadata = {} } = await request.json();
+
+//   try {
+//     const supabase = createRouteHandlerClient({ 
+//       cookies
+//       });      const {
+//       data: { user }
+//     } = await supabase.auth.getUser();
+
+//     const customer = await createOrRetrieveCustomer({
+//       uuid: user?.id || '',
+//       email: user?.email || ''
+//     });
+
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ['card'],
+//       billing_address_collection: 'required',
+//       customer,
+//       line_items: [
+//         {
+//           price: price.id,
+//           quantity
+//         }
+//       ],
+//       mode: 'subscription',
+//       allow_promotion_codes: true,
+//       subscription_data: { items: [{ price: price.id, quantity }], trial_period_days: 7, metadata },
+//       success_url: `${getURL()}/account`,
+//       cancel_url: `${getURL()}/`
+//     });
+
+//     return NextResponse.json({ sessionId: session.id });
+//   } catch (err: any) {
+//     console.log(err);
+//     return new NextResponse('Internal Error', { status: 500 });
+//   }
+// }
+
+
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from "next/headers";
 import { NextResponse } from 'next/server';
@@ -12,9 +62,8 @@ export async function POST(
   const { price, quantity = 1, metadata = {} } = await request.json();
 
   try {
-    const supabase = createRouteHandlerClient({ 
-      cookies
-      });      const {
+    const supabase = createRouteHandlerClient({ cookies });
+    const {
       data: { user }
     } = await supabase.auth.getUser();
 
@@ -35,14 +84,23 @@ export async function POST(
       ],
       mode: 'subscription',
       allow_promotion_codes: true,
-      subscription_data: { items: [{ price: price.id, quantity }], trial_period_days: 7, metadata },
+      subscription_data: {
+        items: [
+          {
+            price: price.id,
+            quantity
+          }
+        ],
+        trial_period_days: 7,
+        metadata
+      },
       success_url: `${getURL()}/account`,
       cancel_url: `${getURL()}/`
     });
 
     return NextResponse.json({ sessionId: session.id });
   } catch (err: any) {
-    console.log(err);
+    console.error(err);
     return new NextResponse('Internal Error', { status: 500 });
   }
 }
