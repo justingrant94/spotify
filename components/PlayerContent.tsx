e111808044290183f5563d9bf6f259eaa0afb184
j-lgrant <justingrant373@gmail.com>
2024-02-06 17:06:33 +0000

    updating pacakages

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -211,12 +211,14 @@
 
 "use client";
 
-import { useSound } from 'use-sound';
 
 import { useEffect, useState } from "react";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+
+
+import useSound = require('use-sound');
 
 import { Song } from "@/types";
 import usePlayer from "@/hooks/usePlayer";
@@ -224,6 +226,9 @@ import usePlayer from "@/hooks/usePlayer";
 import LikeButton from "./LikeButton";
 import MediaItem from "./MediaItem";
 import Slider from "./Slider";
+
+const [play, { pause, sound }] = useSound(songUrl, { volume: volume });
+
 
 
 interface PlayerContentProps {
